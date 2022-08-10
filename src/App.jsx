93a1ef68e8d13dfd1f50f13f3521df8d98b65310
0xgeeb <0xgeeb@gmail.com>
2022-08-10 15:19:12 -0400

    set up project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import "./index.css"
+import Home from "./pages/Home.jsx"
+
+export default function App() {
+    return (
+        <Router>
+            <div className="flex flex-col relative min-w-screen min-h-screen m-0 p-0 overflow-x-hidden">
+                <Routes>
+                    <Route exact path="/" element={<Home />} />
+                </Routes>
+            </div>
+        </Router>
+    )
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import { React, useState } from "react"
+
+export default function Home() {
+  return (
+    <div className="flex justify-center mt-24">
+      <h1 className="mx-auto text-[2.5rem] text-green-300">hello</h1>
+    </div>
+  )
+}
