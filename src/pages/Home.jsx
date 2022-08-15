39ea0aa05a49c8513aee4ac27a59acdde07e6208
0xgeeb <0xgeeb@gmail.com>
2022-08-14 21:19:51 -0400

    set up background image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import { React, useState } from "react"
+import home_image from ".././images/bg2.png"
+import pfp from ".././images/transparent_pfp.png"
 
 export default function Home() {
   return (
-    <div className="flex justify-center mt-24">
-      <h1 className="mx-auto text-[2.5rem] text-green-300">hello</h1>
+    <div className="h-[1050px] flex flex-col" style={{backgroundImage: `url(${home_image})`}} id="background-div">
+      <h1 className="mt-2 ml-6 mx-auto text-[2.5rem]">geeb.dev</h1>
+      <div className="mt-64 mx-auto w-3/5">
+        <img className="h-80 w-80" src={pfp} />
+      </div>
     </div>
   )
 }
