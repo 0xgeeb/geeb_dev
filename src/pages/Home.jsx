e14a39b40f285a19d2de8faca29bd70aeb0f1f9a
0xgeeb <0xgeeb@gmail.com>
2022-08-15 00:11:58 -0400

    added floating avatar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,10 +4,18 @@ import pfp from ".././images/transparent_pfp.png"
 
 export default function Home() {
   return (
-    <div className="h-[1050px] flex flex-col" style={{backgroundImage: `url(${home_image})`}} id="background-div">
-      <h1 className="mt-2 ml-6 mx-auto text-[2.5rem]">geeb.dev</h1>
-      <div className="mt-64 mx-auto w-3/5">
-        <img className="h-80 w-80" src={pfp} />
+    <div>
+      <div className="h-[1050px] flex flex-col" style={{backgroundImage: `url(${home_image})`}} id="background-div">
+        <div className="fixed mt-64 mx-auto left-36" >
+          <img className="h-80 w-80" src={pfp} id="avatar"/>
+        </div>
+        <h1 className="mt-2 ml-6 mx-auto text-[2.5rem]">geeb.dev</h1>
+      </div>
+      <div className="h-[250px]">
+
+      </div>
+      <div className="h-[1050px] flex flex-col" style={{backgroundImage: `url(${home_image})`}} id="background-div">
+
       </div>
     </div>
   )
