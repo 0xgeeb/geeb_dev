4c62b2338e4560aea613fb539fd0f61a4bcc1ee4
0xgeeb <0xgeeb@gmail.com>
2022-08-17 00:16:19 -0400

    did a lil work

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Home from "./pages/Home.jsx"
 export default function App() {
     return (
         <Router>
-            <div className="flex flex-col relative min-w-screen min-h-screen m-0 p-0 overflow-x-hidden">
+            <div className="min-w-screen min-h-screen m-0 p-0 overflow-x-hidden">
                 <Routes>
                     <Route exact path="/" element={<Home />} />
                 </Routes>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,17 +6,22 @@ export default function Home() {
   return (
     <div>
       <div className="h-[1050px] flex flex-col" style={{backgroundImage: `url(${home_image})`}} id="background-div">
-        <div className="fixed mt-64 mx-auto left-36" >
+        <div className="fixed mt-64 mx-auto left-[13%]" >
           <img className="h-80 w-80" src={pfp} id="avatar"/>
         </div>
-        <h1 className="mt-2 ml-6 mx-auto text-[2.5rem]">geeb.dev</h1>
+        {/* <h1 className="fixed right-[35%] top-[35%] text-[2.5rem]" id="heading">geeb.dev</h1> */}
       </div>
-      <div className="h-[250px]">
+      <div className="w-[100%] h-[1250px] bg-slate-200 flex flex-row">
+        <div className="w-[40%] mt-48">
+          
+        </div>
+        <div className="w-[60%] mt-48 flex flex-col">
+          <h1 className="mx-auto">hello</h1>
+        </div>
+      </div>
+      {/* <div className="h-[1050px] flex flex-col" style={{backgroundImage: `url(${home_image})`}} id="background-div">
 
-      </div>
-      <div className="h-[1050px] flex flex-col" style={{backgroundImage: `url(${home_image})`}} id="background-div">
-
-      </div>
+      </div> */}
     </div>
   )
 }
